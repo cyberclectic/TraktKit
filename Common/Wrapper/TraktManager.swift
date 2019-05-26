a10da74e83b34f436735c5e9dab280aac3d3924e
Gerald Wood <gerald.wood@lknlife.com>
2019-05-26 10:51:01 -0400

    Making some things public so can access outside module

--- Common/Wrapper/TraktManager.swift
+++ Common/Wrapper/TraktManager.swift
@@ -35,6 +35,9 @@ public class TraktManager {
     
     // MARK: Internal
     private var staging: Bool?
+    public var clientID: String?
+    public var clientSecret: String?
+    public var redirectURI: String?
     private var baseURL: String?
     private var APIBaseURL: String?
     
