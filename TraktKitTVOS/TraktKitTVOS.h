735dfb34b38e73f141fa6c397746fa6ea1e0fe45
Gerald Wood <gerald.wood@lknlife.com>
2019-05-26 09:47:17 -0400

    Adding TraktKitTVOS Target

---
+++ TraktKitTVOS/TraktKitTVOS.h
@@ -0,0 +1,19 @@
+//
+//  TraktKitTVOS.h
+//  TraktKitTVOS
+//
+//  Created by Gerald Wood on 5/26/19.
+//  Copyright © 2019 Maximilian Litteral. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TraktKitTVOS.
+FOUNDATION_EXPORT double TraktKitTVOSVersionNumber;
+
+//! Project version string for TraktKitTVOS.
+FOUNDATION_EXPORT const unsigned char TraktKitTVOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TraktKitTVOS/PublicHeader.h>
+
+
