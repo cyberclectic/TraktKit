10fcba2ca74287003706e307744e43e9d7f5d765
Gerald Wood <gerald.wood@lknlife.com>
2019-05-27 23:12:16 -0400

    Fixing build warnings
    - Fixing warning
    - Moving public properties to correct MARK:

--- Common/Wrapper/TraktManager.swift
+++ Common/Wrapper/TraktManager.swift
@@ -35,9 +35,6 @@ public class TraktManager {
     
     // MARK: Internal
     private var staging: Bool?
-    public var clientID: String?
-    public var clientSecret: String?
-    public var redirectURI: String?
     private var baseURL: String?
     private var APIBaseURL: String?
     
